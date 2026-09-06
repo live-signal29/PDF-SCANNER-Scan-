9663fd3d05a2a98f28f3af6c9af5c866a8360439
live-signal29 <mybusiness903@gmail.com>
2026-09-06 13:42:00 +0500

    Change applicationId in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.pdfscanner.kxmpzq"
+    applicationId = "com.aistudio.pdfscanner.fxsignallab"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
